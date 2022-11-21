847d0d9bfcf4fb4e30c02ca0e67a125efe8c7813
MuhammadIqbalK <muhammad.iqbal.krisna-2020@vokasi.unair.ac.id>
2022-11-21 19:59:12 +0700

    commit 2

--- app/src/main/java/com/example/dumnofood/setLocation1.kt
+++ app/src/main/java/com/example/dumnofood/setLocation1.kt
@@ -1,7 +1,9 @@
 package com.example.dumnofood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.core.view.WindowCompat
 
 class setLocation1 : AppCompatActivity() {
@@ -10,11 +12,16 @@ class setLocation1 : AppCompatActivity() {
         setContentView(R.layout.activity_set_location1)
         supportActionBar?.hide()
         WindowCompat.setDecorFitsSystemWindows(window, false)
+
+
+    val toHome = findViewById<Button>(R.id.buttonSetloc)
+
+    toHome.setOnClickListener{
+        val intent = Intent(this@setLocation1, navMain::class.java)
+        startActivity(intent)
     }
 
 
-
-
-
+}
 
 }
