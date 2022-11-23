0c179c04b289b170aaec4481575e74c1b77db43c
Muchammad Raharjo Waluyo <mraharjow@gmaul.com>
2022-11-23 16:30:07 +0700

    commit 5

---
+++ app/src/main/java/com/example/dumnofood/DonateFoodActivity.kt
@@ -0,0 +1,13 @@
+package com.example.dumnofood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DonateFoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donatefood)
+
+
+    }
+}
