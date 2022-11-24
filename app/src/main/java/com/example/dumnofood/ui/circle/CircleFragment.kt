34548b1d50ed50e839939c080ff73db8d2afe126
lenovo <mohammadwahyupr@gmail.com>
2022-11-24 09:26:42 +0700

    commit wahyu 1

--- app/src/main/java/com/example/dumnofood/ui/circle/CircleFragment.kt
+++ app/src/main/java/com/example/dumnofood/ui/circle/CircleFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.dumnofood.databinding.FragmentCircleBinding
@@ -23,16 +24,11 @@ class CircleFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val circleViewModel =
-            ViewModelProvider(this).get(CircleViewModel::class.java)
 
         _binding = FragmentCircleBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textCircle
-        circleViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        (activity as AppCompatActivity).supportActionBar?.show()
         return root
     }
 
