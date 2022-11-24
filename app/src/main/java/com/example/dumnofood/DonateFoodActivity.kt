a2cf0e552443e673351683102f2d5d8bb41a901c
Muchammad Raharjo Waluyo <mraharjow@gmaul.com>
2022-11-24 07:15:19 +0700

    commit 5 donatefood intent

--- app/src/main/java/com/example/dumnofood/DonateFoodActivity.kt
+++ app/src/main/java/com/example/dumnofood/DonateFoodActivity.kt
@@ -2,12 +2,14 @@ package com.example.dumnofood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DonateFoodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donatefood)
 
+    val bntOpenFragment : Button
 
     }
 }
